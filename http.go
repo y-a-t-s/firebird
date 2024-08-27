63b3ab4765b6b4492a542257f278d86e165ddcc4
y a t s <140337963+y-a-t-s@users.noreply.github.com>
2024-08-27 11:58:03 -0400

    Check passed hostname

--- http.go
+++ http.go
@@ -7,13 +7,31 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strconv"
 
 	"golang.org/x/net/html"
 	"golang.org/x/net/html/atom"
 )
 
-func parseTags(r io.Reader) (Challenge, error) {
+func splitProtocol(addr string) (proto string, host string, err error) {
+	// FindStringSubmatch is used to capture the groups.
+	// Index 0 is the full matching string with all groups.
+	// The rest are numbered by the order of the opening parens.
+	// Here, we want the last 2 groups (indexes 1 and 2, requiring length 3).
+	tmp := regexp.MustCompile(`^([\w-]+://)?([^/]+)`).FindStringSubmatch(addr)
+	// At the very least, we need the hostname part (index 2).
+	if len(tmp) < 3 || tmp[2] == "" {
+		err = errors.New("Failed to parse address: " + addr)
+		return
+	}
+
+	proto = tmp[1]
+	host = tmp[2]
+	return
+}
+
+func parseTags(r io.Reader) (c Challenge, err error) {
 	parseAttr := func(v string) (uint32, error) {
 		tmp, err := strconv.Atoi(v)
 		if err != nil {
@@ -23,7 +41,6 @@ func parseTags(r io.Reader) (Challenge, error) {
 		return uint32(tmp), nil
 	}
 
-	c := Challenge{}
 	z := html.NewTokenizer(r)
 	for i := z.Next(); i != html.ErrorToken; i = z.Next() {
 		tk := z.Token()
@@ -50,30 +67,45 @@ func parseTags(r io.Reader) (Challenge, error) {
 	}
 
 	if c.Salt == "" {
-		return c, errors.New("Failed to parse challenge from data tags.")
+		err = errors.New("Failed to parse challenge from data tags.")
+		return
 	}
 
-	return c, nil
+	return
 }
 
-func NewChallenge(hc http.Client, host string) (c Challenge, err error) {
-	resp, err := hc.Get(host)
+func NewChallenge(hc http.Client, host string) (Challenge, error) {
+	_, host, err := splitProtocol(host)
 	if err != nil {
-		return c, err
+		return Challenge{}, err
+	}
+
+	resp, err := hc.Get("https://" + host)
+	if err != nil {
+		return Challenge{}, err
 	}
 	defer resp.Body.Close()
 
 	// Check for 203 status
 	if resp.StatusCode != 203 {
-		return c, errors.New("No redirect to challenge page.")
+		return Challenge{}, errors.New("No redirect to challenge page.")
 	}
 
-	c, err = parseTags(resp.Body)
-	return c, err
+	c, err := parseTags(resp.Body)
+	if err != nil {
+		return Challenge{}, err
+	}
+
+	return c, nil
 }
 
 func Submit(hc http.Client, host string, s Solution) (string, error) {
-	resp, err := hc.PostForm(host+"/.sssg/api/answer", url.Values{
+	_, host, err := splitProtocol(host)
+	if err != nil {
+		return "", err
+	}
+
+	resp, err := hc.PostForm("https://"+host+"/.sssg/api/answer", url.Values{
 		"a": []string{s.Salt},
 		"b": []string{fmt.Sprint(s.Nonce)},
 	})
@@ -91,6 +123,7 @@ func Submit(hc http.Client, host string, s Solution) (string, error) {
 		Auth   string
 		Domain string
 	}
+
 	a := auth{}
 	err = json.Unmarshal(b, &a)
 	if err != nil {
